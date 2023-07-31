092703171fa9e8aba7941d0d0b21587fee63489f
Kurgat094 <Kurgat094>
2023-07-31 08:21:19 -0700

    #include "main.h" /** *_strstr - The _strstr() function finds the first occurrence * of the substring needle in the string haystack. * The terminating null bytes (\0) are not compared *@haystack: string where the search is made *@needle: string whose occurence is searched in haystack *Return:Returns a pointer to the beginning of the located * substring, or NULL if the substring is not found. */
    char *_strstr(char *haystack, char *needle)
    {
    int i;
    
    if (*needle == 0)
    return (haystack);
    
    while (*haystack)
    {
    i = 0;
    
    if (haystack[i] == needle[i])
    {
    do {
    if (needle[i + 1] == 0)
    return (haystack);
    i++;
    } while (haystack[i] == needle[i]);
    }
    haystack++;
    }
    return (0);
    }

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,6 +3,8 @@
 *_strstr - The _strstr() function finds the first occurrence
 * of the substring needle in the string haystack.
 * The terminating null bytes (\0) are not compared
+*@haystack: string where the search is made
+*@needle: string whose occurence is searched in haystack
 *Return:Returns a pointer to the beginning of the located
 * substring, or NULL if the substring is not found.
 */
@@ -29,4 +31,4 @@ char *_strstr(char *haystack, char *needle)
 		haystack++;
 	}
 	return ('\0');
-}
+}}
